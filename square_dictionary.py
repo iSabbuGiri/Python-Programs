a149d9419f1e27c8dd6068116b5ad0c8590ae468
Sabbu Giri <sabbugiri50@gmail.com>
2020-11-29 20:53:09 +0545

    pushing programs

---
+++ square_dictionary.py
@@ -0,0 +1,6 @@
+num = int(input("Enter the maximum number:"))
+mydict = dict()
+
+for i in range(1,num+1):
+    mydict[i] = i*i
+print(mydict)    
---
+++ squarebetween1to15.py
@@ -0,0 +1,6 @@
+mydict = dict()
+
+for i in range(1, 16):
+    mydict[i] = i*i
+
+print(mydict)    
